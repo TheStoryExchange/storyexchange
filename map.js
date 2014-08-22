5a40ce2c1ae0ea094074a8abbf467964b51bb570
James Conkling <james.lane.conkling@gmail.com>
2014-08-21 22:52:56 -0400

    init repo

---
+++ map.js
@@ -0,0 +1,4 @@
+
+L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXMtbGFuZS1jb25rbGluZyIsImEiOiJ3RHBOc1BZIn0.edCFqVis7qgHPRgdq0WYsA';
+var map = L.mapbox.map('map', 'james-lane-conkling.5630f970')
+    .setView([41, -95.0], 5);
