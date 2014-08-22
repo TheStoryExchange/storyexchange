95f5332bd92de06805bbe48d24c13c63bb856c72
James Conkling <james.lane.conkling@gmail.com>
2014-08-22 15:09:04 -0400

    add marker colors on load

--- map.js
+++ map.js
@@ -1,27 +1,35 @@
-
 L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXMtbGFuZS1jb25rbGluZyIsImEiOiJ3RHBOc1BZIn0.edCFqVis7qgHPRgdq0WYsA';
 var map = L.mapbox.map('map', 'james-lane-conkling.5630f970')
     .setView([41, -95.0], 5);
 
-var stops = L.mapbox.featureLayer()
-    .loadURL('stops.geojson')
-    .addTo(map)
-    .on('ready',function(){
-        this.eachLayer(function(convo){
-            var p = convo.feature.properties;
-            var img = 'http://media.tumblr.com/dd0f48cfff498f22e356a4af6b332315/tumblr_inline_n3qo2jJPsN1ro2f0s.jpg'
+// an awkward hack so users don't have to define styles in the geojson file
+// see simple style spec: https://github.com/mapbox/simplestyle-spec/tree/master/1.1.0
+var geojson = omnivore.geojson('stops.geojson')
+    .on('ready', function(){
+        this.eachLayer(function(marker){
+            var p = marker.toGeoJSON().properties;
+
+            if(marker.toGeoJSON().properties.status === 'visited'){
+                marker.setIcon(L.mapbox.marker.icon({
+                    'marker-color': '#323232'
+                }));
+            }else{
+                marker.setIcon(L.mapbox.marker.icon({
+                    'marker-color': '#AA6035'
+                }));
+            }
+
+            // set popup
             var content = "<h3 class='title'>" + p.title + "</h3>" +
                 "<div class='desc'>" + p.text + "</div>" +
                 "<div>" +
-                    "<div class='photo'><span class='arrow'></span>click</div>" +
-                    "<img src=" + img + " />" +
+                    // "<div class='photo'><span class='arrow'></span>click</div>" +
+                    "<img src=" + p.photo + " />" +
                 "</div>";
-            convo.bindPopup(content);
-
-            // convo.on('click', function(){
-            //     var latlon = convo.getLatLng();
-            //     map.setView(latlon, 10);
-            // });
+            marker.bindPopup(content);
         });
-    });
+    })
+    .addTo(map);
+
+
 
