efd3c9894bc558356790d6f7200f147dd9950f0b
James Conkling <james.lane.conkling@gmail.com>
2014-08-22 00:22:39 -0400

    add point data

--- map.js
+++ map.js
@@ -2,3 +2,17 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXMtbGFuZS1jb25rbGluZyIsImEiOiJ3RHBOc1BZIn0.edCFqVis7qgHPRgdq0WYsA';
 var map = L.mapbox.map('map', 'james-lane-conkling.5630f970')
     .setView([41, -95.0], 5);
+
+var stops = L.mapbox.featureLayer()
+    .loadURL('stops.geojson')
+    .addTo(map)
+    .on('ready',function(){
+        this.eachLayer(function(stop){
+            stop.on('click', function(){
+                var latlon = stop.getLatLng();
+                console.log(latlon);
+                map.setView(latlon, 10);
+            });
+        });
+    });
+
